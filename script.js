5f2c7aa0d06ba04e6598b206db492ba0fad4e2bc
eddygzz <87101802+eddygzz@users.noreply.github.com>
2021-07-22 17:45:10 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu () {
+	var menu = document.getElementById ("hidden-menu")
+	if (menu.style.display == "none") {
+		menu.style.display = "block"
+
+	}else {
+		menu.style.display = "none"
+	}
+}
